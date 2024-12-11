a13bb9de82f0f89bc2b1ac40983de369c4712915
Ktrzyna23 <kacperxiii@gmail.com>
2024-12-11 18:50:38 +0100

    mamy trójkąt

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # the open-source pygame library
 # throughout this file
 import pygame
-
+from player import *
 from constants import *
 def main():
     print("Starting asteroids!")
@@ -12,11 +12,13 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick()/1000
 if __name__ == "__main__":
