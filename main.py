bf0bade99e8893bd27510f6ac5d6dce7a9746c8b
Ktrzyna23 <kacperxiii@gmail.com>
2024-12-11 18:09:15 +0100

    Dodawanie fpsów

--- main.py
+++ main.py
@@ -2,19 +2,22 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+
 from constants import *
 def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    pygame.init
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    BLACK = (0, 0, 0,)
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-    screen.fill(BLACK)
-    pygame.display.flip()
+        screen.fill("black")
+        pygame.display.flip()
+        dt = clock.tick()/1000
 if __name__ == "__main__":
     main()
